78fd40edd62e361495a9de36813fc339e6afa964
Ashok Ankamreddy <ashok@Ashoks-MacBook-Pro.local>
2023-09-12 10:27:17 +0530

    [Prasanth|Ashok] Add. Centimeter check logic

---
+++ src/main/java/org/tw/unitcheck/Centimter.java
@@ -0,0 +1,11 @@
+package org.tw.unitcheck;
+
+public class Centimter {
+    public boolean checkObjectsAreSame(String value1, String value2) {
+        return value1 == value2;
+    }
+
+    public boolean checkObjectsAreEqual(String value1, String value2) {
+        return value1.equals(value2);
+    }
+}
